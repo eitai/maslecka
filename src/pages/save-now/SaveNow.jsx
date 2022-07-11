0363ab44d5b4c6704814bf3be63ffeb87daf48e6
eitai <eitaimeir@gmail.com>
2022-07-12 00:53:32 +0300

    done  save now

--- src/pages/save-now/SaveNow.jsx
+++ src/pages/save-now/SaveNow.jsx
@@ -6,8 +6,9 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import TextField from '@mui/material/TextField';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
-import { GiSplitArrows } from 'react-icons/gi';
 import emailjs from 'emailjs-com';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
@@ -23,11 +24,12 @@ const validationSchema = yup.object().shape({
 const inputStyle = {
   width: '100%',
   mb: 3,
+  border: '1px solid black',
+  borderRadius: '5px',
 };
 
 const SaveNow = () => {
-  const [mainImg, setMainImg] = useState(1);
-  const [subject, setSubject] = useState('כללי');
+  const [subjects, setSubjects] = useState([]);
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [textArea, setTextArea] = useState('');
@@ -40,11 +42,6 @@ const SaveNow = () => {
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: yupResolver(validationSchema) });
-
-  const handleSubject = (imgNum, subject) => {
-    setMainImg(imgNum);
-    setSubject(subject);
-  };
 
   const sendEmail = (details) => {
     emailjs
@@ -70,12 +67,24 @@ const SaveNow = () => {
       email: e.email,
       phone: e.phone,
       textArea: textArea,
-      subject: subject,
+      subject: subjects,
     };
     debugger;
     sendEmail(userMessage);
   };
 
+  const handleChosenSubjects = (subject) => {
+    const subjectExist = subjects.includes(subject);
+    if (!subjectExist) {
+      setSubjects((prev) => [...prev, subject]);
+    } else {
+      const subjectIndex = subjects.indexOf(subject);
+      const newSubjects = subjects;
+      newSubjects.splice(subjectIndex, 1);
+      setSubjects([...newSubjects]);
+    }
+  };
+  console.log(subjects);
   return (
     <div>
       <Navbar isBackgroundColorOn={true} />
@@ -88,133 +97,124 @@ const SaveNow = () => {
           </h2>
         </div>
         <div className={`container ${Style.container}`}>
-          <div className={Style.form_container}>
-            <form
-              ref={form}
-              className={Style.form}
-              onSubmit={handleSubmit(handleFormSubmit)}
-            >
-              <TextField
-                error={errors?.fullname}
-                label='שם מלא'
-                type='text'
-                helperText={errors.fullname ? errors.fullname.message : ''}
-                {...register('fullname')}
-                className={Style.input}
-                size='small'
-                sx={inputStyle}
-                name='fullname'
-                onChange={(e) => setFullName(e.target.value)}
-                value={fullName}
+          <form
+            ref={form}
+            className={Style.form}
+            onSubmit={handleSubmit(handleFormSubmit)}
+          >
+            <TextField
+              error={errors?.fullname}
+              label='שם מלא'
+              type='text'
+              helperText={errors.fullname ? errors.fullname.message : ''}
+              {...register('fullname')}
+              className={Style.input}
+              size='small'
+              sx={inputStyle}
+              name='fullname'
+              onChange={(e) => setFullName(e.target.value)}
+              value={fullName}
+            />
+            <TextField
+              error={errors?.email}
+              label='אימייל'
+              type='email'
+              helperText={errors.email ? errors.email.message : ''}
+              {...register('email')}
+              className={Style.input}
+              size='small'
+              sx={inputStyle}
+              name='email'
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <TextField
+              error={errors.phone}
+              label='טלפון'
+              type='text'
+              className={Style.input}
+              helperText={errors.phone ? errors.phone.message : ''}
+              size='small'
+              sx={inputStyle}
+              name='phone'
+              {...register('phone')}
+              onChange={(e) => setPhone(e.target.value)}
+              value={phone}
+            />
+            <TextareaAutosize
+              maxRows={4}
+              aria-label='maximum height'
+              placeholder='שתפו אותנו'
+              name='textArea'
+              sx={inputStyle}
+              style={{
+                width: '25rem',
+                border: '1px solid black',
+                height: 70,
+                display: 'block',
+                marginBottom: '15px',
+                color: 'rgba(0, 0, 0, 1)',
+                paddingRight: '10px',
+              }}
+              onChange={(e) => setTextArea(e.target.value)}
+              value={textArea}
+            />
+            <input type='submit' value='שלח' className={Style.btn_signup} />
+          </form>
+          <div className={Style.img_container}>
+            <span className={Style.img_title}>בחרו נושאי פנייה</span>
+            <div className={`save-now-mui ${Style.checkboxes}`}>
+              <FormControlLabel
+                control={<Checkbox />}
+                label='ביטוחים ופיננסים'
+                onChange={(e) => handleChosenSubjects('ביטוחים ופיננסים')}
               />
-              <TextField
-                error={errors?.email}
-                label='אימייל'
-                type='email'
-                helperText={errors.email ? errors.email.message : ''}
-                {...register('email')}
-                className={Style.input}
-                size='small'
-                sx={inputStyle}
-                name='email'
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
+              <FormControlLabel
+                control={<Checkbox />}
+                label='דיור'
+                onChange={(e) => handleChosenSubjects('דיור')}
               />
-              <TextField
-                error={errors.phone}
-                label='טלפון'
-                type='text'
-                className={Style.input}
-                helperText={errors.phone ? errors.phone.message : ''}
-                size='small'
-                sx={inputStyle}
-                name='phone'
-                {...register('phone')}
-                onChange={(e) => setPhone(e.target.value)}
-                value={phone}
+              <FormControlLabel
+                control={<Checkbox />}
+                label='תקשורת'
+                onChange={(e) => handleChosenSubjects('תקשורת')}
               />
-              <TextareaAutosize
-                maxRows={4}
-                aria-label='maximum height'
-                placeholder='שתפו אותנו'
-                name='textArea'
-                style={{
-                  width: '25rem',
-                  border: '1px solid rgba(0, 0, 0, 0.23)',
-                  height: 70,
-                  display: 'block',
-                  marginBottom: '15px',
-                  color: 'rgba(0, 0, 0, 0.6)',
-                  paddingRight: '10px',
-                }}
-                onChange={(e) => setTextArea(e.target.value)}
-                value={textArea}
+              <FormControlLabel
+                control={<Checkbox />}
+                label='מזון וקניות'
+                onChange={(e) => handleChosenSubjects('מזון וקניות')}
               />
-              <input type='submit' value='שלח' className={Style.btn_signup} />
-            </form>
-            <div className={Style.img_container}>
-              <span className={Style.img_title}> בחרתם ב - </span>
-              <span className={Style.img_subtitle}> {subject}</span>
-              <img
-                src={require(`../../assets/contactus${mainImg}.webp`)}
-                alt='logo'
-                className={Style.img}
+              <FormControlLabel
+                control={<Checkbox />}
+                label='טיפוח'
+                onChange={(e) => handleChosenSubjects('טיפוח')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label='ביגוד'
+                onChange={(e) => handleChosenSubjects('ביגוד')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label='ביטוח'
+                onChange={(e) => handleChosenSubjects('ביטוח')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label='רכב ונסיעות'
+                onChange={(e) => handleChosenSubjects('רכב ונסיעות')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label='ילדים וחינוך'
+                onChange={(e) => handleChosenSubjects('ילדים וחינוך')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label='בריאות'
+                onChange={(e) => handleChosenSubjects('בריאות')}
               />
             </div>
-          </div>
-          <div className={Style.subtitle}>
-            <span>בחרו נושא פנייה</span>
-            <div className={Style.icon}>
-              <GiSplitArrows />
-            </div>
-          </div>
-          <div className={Style.choice_section}>
-            <div className={Style.line}></div>
-
-            <button
-              className={Style.box}
-              onClick={() => handleSubject(1, 'ביטוח')}
-            >
-              <span className={Style.box_text}>ביטוח</span>
-              <img
-                src={require('../../assets/contactus1.webp')}
-                alt='logo'
-                className={Style.img}
-              />
-            </button>
-            <button
-              className={Style.box}
-              onClick={() => handleSubject(2, 'ביטוח')}
-            >
-              <span className={Style.box_text}>ביטוח</span>
-              <img
-                src={require('../../assets/contactus2.webp')}
-                alt='logo'
-                className={Style.img}
-              />
-            </button>
-            <button
-              className={Style.box}
-              onClick={() => handleSubject(3, 'חיסכון')}
-            >
-              <span className={Style.box_text}>חיסכון</span>
-              <img
-                src={require('../../assets/contactus3.webp')}
-                alt='logo'
-                className={Style.img}
-              />
-            </button>
-            <button
-              className={Style.box}
-              onClick={() => handleSubject(4, 'פנסיה')}
-            >
-              <span className={Style.box_text}>פנסיה</span>
-              <img
-                src={require('../../assets/contactus4.webp')}
-                alt='logo'
-                className={Style.img}
-              />
-            </button>
           </div>
         </div>
       </div>
